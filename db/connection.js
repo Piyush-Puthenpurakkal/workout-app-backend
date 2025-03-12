a93a7c531f862a85ea2e6b202bb0ef323fc7de06
Piyush Baburaj <piyushbaburj98@gmail.com>
2025-03-12 22:50:50 +0530

    Using Atlas instead of local mogodb

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/Project")
+  // .connect("mongodb://localhost:27017/Project")
+  .connect(
+    "mongodb+srv://piyushbaburaj98:IuOlpMUGmY3TY13S@cluster0.av3nv.mongodb.net/WorkoutApp?retryWrites=true&w=majority&appName=Cluster0"
+  )
   .then(() => {
-    console.log("Connection Established");
+    console.log("MongoDB Connection is Established");
   })
   .catch((err) => {
     console.log("Error is :${err}");
